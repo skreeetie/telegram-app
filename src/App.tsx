6efebbf588cb15ef215b289800885442185898b3
Artem <skreeetie@gmail.com>
2025-11-15 03:38:24 +0300

    recharts

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,30 @@ import { useGetBotQuery, useSendMessageMutation } from "./api/botApi";
 import { Box, Button, TextField } from "@mui/material";
 import WebApp from "@twa-dev/sdk";
 import "./index.scss";
+import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from "recharts";
+
+const barData = [
+  {
+    name: 'Alex',
+    loginCount: 24,
+  },
+  {
+    name: 'Michael',
+    loginCount: 4,
+  },
+  {
+    name: 'Prompix',
+    loginCount: 18,
+  },
+  {
+    name: 'Artem',
+    loginCount: 32,
+  },
+]
 
 export const App = () => {
   const { data: botInfo, isSuccess } = useGetBotQuery();
+  const [isAnimationActive, setIsAnimationActive] = useState(true);
   const [sendMessage] = useSendMessageMutation();
   const [value, setValue] = useState("");
   const handleChange = ({
@@ -19,6 +40,9 @@ export const App = () => {
   };
   useEffect(() => {
     WebApp.ready();
+    setTimeout(() => {
+      setIsAnimationActive(false);
+    }, 1000)
   }, []);
   useEffect(() => {
     if (isSuccess) console.log(botInfo);
@@ -38,6 +62,14 @@ export const App = () => {
       <Button size="large" onClick={handleSend}>
         Send Message
       </Button>
+      <BarChart style={{width: '100%', maxWidth: '400px', maxHeight: '70vh', aspectRatio: '0.5'}} data={barData} responsive>
+        <CartesianGrid strokeDasharray='3 6' />
+        <XAxis dataKey='name' />
+        <YAxis width='auto' />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey='loginCount' fill="#8884d8" isAnimationActive={isAnimationActive} />
+      </BarChart>
     </Box>
   );
 };
